c378002df237e74d9a1deccbd7e579f7d6abf935
Vivek Gupta <vivekgupta3.vg@gmail.com>
2018-03-29 15:59:33 +0530

    vivek's code

--- Assignment 5/Ass5_1.cpp
+++ Assignment 5/Ass5_1.cpp
@@ -1,35 +1,15 @@
-#include <bits/stdc++.h>
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-using namespace std;
+#define INTERRUPT 10
 
 // data structures
 int page_table[64];
 int *page_frame;
-queue <int> myfifo; 
 
-int FIFO(){
-	int temp = myfifo.front(); 
-	myfifo.pop();
-	return temp;
-}
-
-int secondChance(){
-	int page_probe;
-
-	while(1){
-		page_probe = myfifo.front(); 
-		if((page_table[page_probe] & 1) == 0){
-			myfifo.pop();
-			return page_probe;
-		}
-		else{
-			page_table[page_probe] ^= 1;
-			myfifo.pop();
-			myfifo.push(page_probe);
-		}
-	}
-}
+int time_used[64];
+int counter;
 
 int getFrame(int m){
     int i;
@@ -39,6 +19,51 @@ int getFrame(int m){
             return i;
     }
     return -1;
+}
+
+int random(){
+	do{
+		i = rand()%64;
+	}while(page_table[i] & 100 == 0);
+	return i;
+}
+
+int lru(){
+	int m=counter,i,page_no=-1;
+	for(i=0;i<64;i++){
+		
+		if((page_table[i] & 100==1) && (time_used[i]<=m)){
+			m=time_used[i];
+			page_no = i;
+		}
+	}
+	return page_no;
+}
+
+int nru(){
+	vector<int> v0,v1,v2,v3;
+	int i;
+	for(i=0;i<64;i++){
+		switch((page_table[i] & 3)){
+			case 0:	v0.push_back(i);
+					break;
+			case 1: v2.push_back(i);
+					break;
+			case 2: v1.push_back(i);
+					break;
+			case 3: v3.push_back(i);
+					break;						
+		
+		}
+	}
+	if(v0.size()>0)
+		return v0[rand()%(v0.size())];
+	if(v1.size()>0)
+		return v1[rand()%(v1.size())];
+	if(v2.size()>0)
+		return v2[rand()%(v2.size())];
+	if(v3.size()>0)
+		return v3[rand()%(v3.size())];
 }
 
 void printPageTable(){
@@ -53,10 +78,10 @@ void printPageTable(){
 
 }
 
-void readFile(const char *filename, int m, int mode){
+void readFile(char *filename){
     FILE *fp;
     char line[50];
-    int page_no, page_frame, victim_page, line_no=0, page_probe;
+    int page_no,page_frame,victim_page,line_no=0;
 
     long long cost=0;
     int page_faults=0,page_transfers=0;
@@ -65,8 +90,16 @@ void readFile(const char *filename, int m, int mode){
 
     while(fgets(line, sizeof(line), fp) != NULL){
         line_no++;
+        if(line_no%INTERRUPT==0){
+        	for(i=0;i<64;i++){
+        		page_table[i]>>1;
+        	    page_table[i]<<1;
+        	}
+        }
         if(line[0]=='#')continue;
         page_no = atoi(line+2);
+        time_used[page_no] = counter;
+        counter++;
         switch(line[0]){
             case '0':   if(page_table[page_no] & 4 == 1){
                             // valid page
@@ -75,25 +108,15 @@ void readFile(const char *filename, int m, int mode){
                             printPageTable();
                         }else{
                             // invalid page
-                            page_frame = getFrame(m);
+                            page_frame = getFrame();
                             page_faults += 1;
                             if(page_frame==-1){
                                 // memory full
-
-                                switch(mode){
-                                	case 0:	victim_page = FIFO(); break;
-                                	/*case 1:	victim_page = (); break;
-                                	case 2:	victim_page = (); break;*/
-                                	case 3:	break;
-                                	case 4:	victim_page = secondChance(); break;
-                                }
-                                
+                                // victim_page = fifo();
                             	page_frame = page_table[victim_page]>>3;
                             	page_table[page_no] = page_frame<<3;
                             	page_table[victim_page] ^= 4;
                                 page_table[page_no] |= 5;
-                                
-                                myfifo.push(page_no);
 
                             	printf("Output generated:\n  %d: UNMAP     %d  %d\n",line_no,victim_page,page_frame);
 
@@ -114,8 +137,6 @@ void readFile(const char *filename, int m, int mode){
 
                             }else{
                                 // frame available
-                            	myfifo.push(page_no);
-
                                 page_transfers += 1;
                                 cost += 3251;
 
@@ -134,24 +155,14 @@ void readFile(const char *filename, int m, int mode){
                         }else{
                             // invalid page
                             page_faults += 1;
-                            page_frame = getFrame(m);
+                            page_frame = getFrame();
                             if(page_frame==-1){
                                 // memory full
-
-                                switch(mode){
-                                	case 0:	victim_page = FIFO(); break;
-                                	/*case 1:	victim_page = (); break;
-                                	case 2:	victim_page = (); break;
-                                	case 3:	victim_page = (); break;*/
-                                	case 4:	victim_page = secondChance(); break;
-                                }
-
+                                // victim_page = fifo();
                             	page_frame = page_table[victim_page]>>3;
                             	page_table[page_no] = page_frame<<3;
                             	page_table[victim_page] ^= 4;
                                 page_table[page_no] |= 7;
-                                
-                                myfifo.push(page_no);
 
                             	printf("Output generated:\n  %d: UNMAP     %d  %d\n",line_no,victim_page,page_frame);
 
@@ -172,8 +183,7 @@ void readFile(const char *filename, int m, int mode){
 
                             }else{
                                 // frame available
-                                myfifo.push(page_no);
-
+                                
                                 page_transfers += 1;
                                 cost += 3251;
 
@@ -189,9 +199,12 @@ void readFile(const char *filename, int m, int mode){
 
 int main()
 {
-	int m;
-	cout<<"Enter size of memory\n";
-	cin>>m;
-    readFile("mytest.txt", m, 0);
+	srand(time(0));
+
+	counter=0;
+	for(i=0;i<64;i++)
+		time_used[i]=-1;
+
+    readFile("mytest.txt");
     return 0;
 }
